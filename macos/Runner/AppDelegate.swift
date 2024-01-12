8642130756696044251dacbbcbebfb704394a96f
SamadiPour <samadipoor2@gmail.com>
2024-01-12 12:22:45 +0330

    feat(macos): allow app to be in the background

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,31 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        changeDockIcon(showIcon: false)
+        return false
+    }
+
+    override func applicationShouldHandleReopen(_: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag {
+            for window in NSApp.windows {
+                if !window.isVisible {
+                    window.setIsVisible(true)
+                }
+                window.makeKeyAndOrderFront(self)
+                NSApp.activate(ignoringOtherApps: true)
+            }
+        }
+        changeDockIcon(showIcon: true)
+        return true
+    }
+
+    func changeDockIcon(showIcon state: Bool) {
+        if state {
+            NSApp.setActivationPolicy(NSApplication.ActivationPolicy.regular)
+        }
+        else {
+            NSApp.setActivationPolicy(NSApplication.ActivationPolicy.accessory)
+        }
+    }
 }
