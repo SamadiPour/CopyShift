7ec0c4b82abd0c143c9ef95d9046a0ef646738ea
SamadiPour <samadipoor2@gmail.com>
2024-01-19 15:55:08 +0330

    refactor: clipboard transformer to text transformer
    To put the responsibility of keeping the list onto other classes

--- lib/logic/services/text_transformer.dart
+++ lib/logic/services/text_transformer.dart
@@ -1,15 +1,16 @@
 import 'package:copy_shift/models/transform_item.dart';
 
-class ClipboardTransformer {
-  final List<TransformItem> _transformItems = [];
-
+class TextTransformer {
   /// Transforms the input string based on the list of rules.
   ///
-  /// @param input The string to be transformed.
-  /// @return The transformed string.
-  // todo - override transform items
-  String transform(String input) {
-    for (final item in _transformItems) {
+  /// input: The string to be transformed.
+  /// transformers: The list of rules to be applied.
+  /// it return The transformed string.
+  static String transform(
+    String input, {
+    required List<TransformItem> transformers,
+  }) {
+    for (final item in transformers) {
       // If enabled and match the pattern
       if (item.enabled && RegExp(item.matchPattern).hasMatch(input)) {
         // Replace matched pattern
@@ -24,10 +25,4 @@ class ClipboardTransformer {
     // Return the transformed string
     return input;
   }
-
-  void load() {}
-
-  void update(TransformItem items) {}
-
-  void remove(String id) {}
 }
